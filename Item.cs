0d425cbdfc7d970779964689b7ea0bf82acc7e6f
pteaford8 <57969682+pteaford8@users.noreply.github.com>
2020-05-27 13:31:35 -0700

    Add files via upload
    Module Completed

---
+++ Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BucketList.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
